78dcc015dbd0cff8a74a2f5845ee69dbc4795d8b
Precious Idam <pidam@keystonebankng.com>
2020-11-12 15:13:23 +0100

    corrected package name

--- harrp/asgi.py
+++ harrp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for harpp project.
+ASGI config for harrp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harpp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harrp.settings')
 
 application = get_asgi_application()
